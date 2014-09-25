7fa656ed077247c0933719789a1d5ef95bdb8235
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2014-09-25 20:13:05 +0200

    Added a proper folder structure

---
+++ meteor/client/views/dbview/dbview.js
@@ -0,0 +1,8 @@
+
+Template.dbview.helpers({
+  /* Dummy collection subscription
+  comments: function() {
+    return Comments.find({postId: this._id});
+  }
+  */
+});
--- meteor/views/dbview/dbview.js
+++
@@ -1,9 +0,0 @@
-if (Meteor.isClient){ 
-	Template.dbview.helpers({
-	  /* Dummy collection subscription
-	  comments: function() {
-	    return Comments.find({postId: this._id});
-	  }
-	  */
-	});
-}

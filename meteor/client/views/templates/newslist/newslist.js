30ae3fc1cbe7f3e8735bf68345e9d6f2d13f9c05
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2014-09-28 11:34:57 +0200

    Added news count function

--- meteor/client/views/templates/newslist/newslist.js
+++ meteor/client/views/templates/newslist/newslist.js
@@ -1,4 +1,9 @@
 // Kunne godt tænke mig at finde en måde at gøre denne her global på alle content, så man ikke skal skrive den i hver template.js fil :) 
-Template.newslist.children = function() {
-	return Content.find({ Parent: 1003 });
-}
+Template.newslist.helpers({
+  children: function() {
+	 return Content.find({ Parent: 1003 });
+  },
+  newsCount: function(){
+    return Content.find({ Parent: 1003 }).count();
+  }
+});

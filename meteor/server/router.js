0f337d184d413d51862c8c0ac9cd369b53254290
Design Monkey <peter@designmonkey.dk>
2014-10-03 09:42:24 +0200

    Route testing - Insert working
    Update not working :)

--- meteor/server/router.js
+++ meteor/server/router.js
@@ -7,10 +7,26 @@ Router.map(function() {
             // GET, POST, PUT, DELETE
             var requestMethod = this.request.method;
             // Data from a POST request
-            var requestData = this.request.body;
+            var data = this.request.body;
             // Could be, e.g. application/xml, etc.
             this.response.writeHead(200, {'Content-Type': 'text/html'});
-            console.log(requestData);
+            console.log(data);
+            console.log(Content.findOne({id: data.Id }));
+            if(Content.findOne({id: data.Id }) == undefined) {
+                var newId = Content.insert(data);
+                this.response.end('Inserted: ' + newId);
+            }
+            else {
+
+                Content.update({ id: data.Id }, {$set: data}, function(error) {
+                  if (error) {
+                    this.response.end('Update error: ');    
+                  } else {
+                    this.response.end('Updated');    
+                  }
+                });
+
+            }
             // Lav API key checker og sorter API key fra :) 
             // Lav her søg efter Content.find({ id: requestData.id }) og så update eller insert
             //this.response.end('Testing: ' + JSON.stringify(requestData));
